55ef43a62b2a18298564d94278f15ca9c0f4591e
nat <87210044+fokai7@users.noreply.github.com>
2021-10-20 16:51:58 -0700

    Fixed music manager

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,32 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
+    //Music source and tracks
     public AudioClip[] musicClips;
     public AudioSource musicSrc;
 
-    // Start is called before the first frame update
+    // Don't destroy gameobject
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
     }
 
+    //Plays first index track since technically
+    //no level was loaded at start.
     void Start()
     {
         musicSrc = GetComponent<AudioSource>();
+        PlayMusic(SceneManager.GetActiveScene().buildIndex);
     }
 
-    private void OnLevelWasLoaded(int level)
+    //OnLevelWasLoaded only works for scene changes
+    //AFTER game is first loaded.
+    //This will play any further scene changes.
+    private void OnLevelWasLoaded(int levelIndex)
     {
-        AudioClip levelMusic = musicClips[level];
-        Debug.Log("Playing song: " + name);
+        PlayMusic(levelIndex);
+    }
+
+    //Plays music track according to level index
+    void PlayMusic(int songToChangeTo)
+    {
+        
+        AudioClip levelMusic = musicClips[songToChangeTo];
         if (levelMusic)
         {
-            musicSrc.clip = levelMusic;
+            musicSrc.clip = musicClips[songToChangeTo];
             musicSrc.loop = true;
             musicSrc.Play();
         }
+        Debug.Log("Playing song: " + songToChangeTo);
     }
 }
