4877eccd78536356fa90cd3ce687137644e8e215
darthvader99999 <bhow245@gmail.com>
2021-10-25 22:29:44 -0700

    script kinda complete added some clickable object methods

--- Assets/Scripts/ClickableObject.cs
+++ Assets/Scripts/ClickableObject.cs
@@ -8,10 +8,33 @@ using Yarn.Unity;
 public class ClickableObject : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private string node;
+    private bool clickable;
+    private SpriteRenderer img;
     
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("clicked on object");
         TransitionManager.Instance.dialogueRunner.StartDialogue(node);
+        img = GetComponent<SpriteRenderer>();
+    }
+    [YarnCommand("SetObjectClickable")]
+    public void SetObjectClickable()
+    {
+        clickable = true;
+    }
+    [YarnCommand("FadeInObject")]
+    public void FadeInObject()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        img.color = new Color(0, 0, 0, 1);
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            img.color = new Color(0, 0, 0, i);
+            yield return null;
+        }
     }
 }
